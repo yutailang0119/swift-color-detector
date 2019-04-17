ddafa5a47219c90fcf269666b1b05d4cbb54e6a0
yutailang0119 <muta.yutaro@gmail.com>
2019-04-17 23:21:09 +0900

    Fix RGBColorSyntaxRewriter to adopt to RGBDetectable

--- Sources/SwiftColorDetectKit/RGBColorSyntaxRewriter.swift
+++ Sources/SwiftColorDetectKit/RGBColorSyntaxRewriter.swift
@@ -8,47 +8,14 @@
 import Foundation
 import SwiftSyntax
 
-internal class RGBColorSyntaxRewriter: SyntaxRewriter {
+internal class RGBColorSyntaxRewriter: SyntaxRewriter, RGBColorDetectable {
 
     override func visit(_ node: FunctionCallExprSyntax) -> ExprSyntax {
-        guard isUIColorInitializer(of: node) else {
+        guard let rgbColor = detectRGBColor(from: node) else {
             return node
         }
 
-        return rewriteColorFunctionCallExprSyntax(node)
-    }
-
-    private func isUIColorInitializer(of node: FunctionCallExprSyntax) -> Bool {
-        return
-            (node.calledExpression.description == "UIColor" && node.leftParen != nil) // UIColor(red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat)
-            ||
-            node.calledExpression.description == "UIColor.init" // UIColor.init(red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat)
-    }
-
-    private func rewriteColorFunctionCallExprSyntax(_ node: FunctionCallExprSyntax) -> ExprSyntax {
-        var red, green, blue, alpha: CGFloat?
-        node.argumentList.forEach { argumentSyntax in
-            guard let label = argumentSyntax.label else {
-                return
-            }
-            switch label.text {
-            case "red":
-                red = CGFloat(expression: argumentSyntax.expression)
-            case "green":
-                green = CGFloat(expression: argumentSyntax.expression)
-            case "blue":
-                blue = CGFloat(expression: argumentSyntax.expression)
-            case "alpha":
-                alpha = CGFloat(expression: argumentSyntax.expression)
-            default:
-                break
-            }
-        }
-        guard let rgbcolor = RGBColor(red: red, green: green, blue: blue, alpha: alpha) else {
-            return node
-
-        }
-        return node.withArgumentList(rgbcolor.hexInitializerArgumentListSyntax)
+        return node.withArgumentList(rgbColor.hexInitializerArgumentListSyntax)
     }
 
 }
