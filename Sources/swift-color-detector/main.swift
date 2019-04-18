820a93ed72d575425d18f42dbf2a1164d16374be
yutailang0119 <muta.yutaro@gmail.com>
2019-04-18 19:10:06 +0900

    Fix to support recursive execution

--- Sources/swift-color-detector/main.swift
+++ Sources/swift-color-detector/main.swift
@@ -1,32 +1,12 @@
-import SwiftColorDetectKit
+
+import Foundation
+import FileScanKit
 
 let arguments = CommandLine.arguments
 
-guard let subcommand = arguments.dropFirst().first else {
+guard let commandName = arguments.dropFirst().first else {
     fatalError("Error: Indeclear arguments")
 }
-
-let path = arguments.dropFirst().dropFirst().first
-
-switch subcommand {
-case "help":
-    let documents = """
-    dump \\(path): Dump code
-    rewrite \\(path): Add HexColor Comments and Save
-    """
-    print(documents)
-case "dump":
-    guard let path = path else {
-        fatalError("Error: Indeclear file path")
-    }
-    let target = Target(path: path)
-    try target.detect()
-case "rewrite":
-    guard let path = path else {
-        fatalError("Error: Indeclear file path")
-    }
-    let target = Target(path: path)
-    try target.rewrite()
-default:
-    fatalError("Error: Indeclear arguments")
-}
+let path = arguments.dropFirst(2).first
+let subCommand = SubCommand(name: commandName, path: path)
+try subCommand.excute()
